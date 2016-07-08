d40bfdbab74ad3a4dde8f1dc8dd6bd842e7f7480
yokouei <yokouei@gmail.com>
2016-07-08 13:42:30 +0900

    example-1-03.php

--- Chapter1/example-1-02.php
+++ Chapter1/example-1-02.php
@@ -1,4 +1,4 @@
-<form method="POST" action="sayhello.php">
+<form method="POST" action="example-1-03.php">
 Your Name: <input type="text" name="user">
 <br/>
 <input type="submit" value="Say Hello">
